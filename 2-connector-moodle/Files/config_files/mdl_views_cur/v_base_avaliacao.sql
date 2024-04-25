0683a64f4f2951350fe0907bb47cd43c7f465d50
Thiago <thiago@calriz.com>
2024-04-25 17:05:30 -0300

    fix: Update base avaliacao variables

--- 2-connector-moodle/Files/config_files/mdl_views_cur/v_base_avaliacao.sql
+++ 2-connector-moodle/Files/config_files/mdl_views_cur/v_base_avaliacao.sql
@@ -9,5 +9,5 @@
 
   UNION ALL
 
-  SELECT *, 2023 ano_base
-  FROM `[prj_id_lnd].[dts_nm_lnd].base_avaliacao_f_2023`
+  -- SELECT *, 2023 ano_base
+  -- FROM `[prj_id_lnd].[dts_nm_lnd].base_avaliacao_f_2023`
--- 2-connector-moodle/Files/config_files/mdl_views_cur/v_base_avaliacao_norm.sql
+++ 2-connector-moodle/Files/config_files/mdl_views_cur/v_base_avaliacao_norm.sql
@@ -3,19 +3,19 @@
 WITH
 
 b1 AS (
-  SELECT id, matricula, ano_base, serie, bimestre_1 nota, 1 bimestre FROM `[prj_id_bq_cur].[dts_nm_cur].v_base_avaliacao`
+  SELECT id, matricula, ano_base, serie, bimestre_1 nota, 1 bimestre FROM `sme-poc-edp-dwh-cur.base_avaliacao_curated.v_base_avaliacao`
 ),
 
 b2 AS (
-  SELECT id, matricula, ano_base, serie, bimestre_2 nota, 2 bimestre FROM `[prj_id_bq_cur].[dts_nm_cur].v_base_avaliacao`
+  SELECT id, matricula, ano_base, serie, bimestre_2 nota, 2 bimestre FROM `sme-poc-edp-dwh-cur.base_avaliacao_curated.v_base_avaliacao`
 ),
 
 b3 AS (
-  SELECT id, matricula, ano_base, serie, bimestre_3 nota, 3 bimestre FROM `[prj_id_bq_cur].[dts_nm_cur].v_base_avaliacao`
+  SELECT id, matricula, ano_base, serie, bimestre_3 nota, 3 bimestre FROM `sme-poc-edp-dwh-cur.base_avaliacao_curated.v_base_avaliacao`
 ),
 
 b4 AS (
-  SELECT id, matricula, ano_base, serie, bimestre_4 nota, 4 bimestre FROM `[prj_id_bq_cur].[dts_nm_cur].v_base_avaliacao`
+  SELECT id, matricula, ano_base, serie, bimestre_4 nota, 4 bimestre FROM `sme-poc-edp-dwh-cur.base_avaliacao_curated.v_base_avaliacao`
 )
 
 SELECT b1.* FROM b1
