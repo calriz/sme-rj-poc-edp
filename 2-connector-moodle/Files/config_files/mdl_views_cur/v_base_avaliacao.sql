87514e89b54cbad8d9580b9db1028248a41de9be
Thiago <thiago@calriz.com>
2024-04-26 11:12:51 -0300

    chore: Update tables

--- 2-connector-moodle/Files/config_files/mdl_views_cur/v_base_avaliacao.sql
+++ 2-connector-moodle/Files/config_files/mdl_views_cur/v_base_avaliacao.sql
@@ -7,7 +7,7 @@
     SELECT *, 2022 ano_base
   FROM `[prj_id_lnd].[dts_nm_lnd].base_avaliacao_f_2022`
 
-  -- UNION ALL
+ UNION ALL
 
-  -- SELECT *, 2023 ano_base
-  -- FROM `[prj_id_lnd].[dts_nm_lnd].base_avaliacao_f_2023`
+SELECT *, 2023 ano_base
+  FROM `[prj_id_lnd].[dts_nm_lnd].base_avaliacao_f_2023`
